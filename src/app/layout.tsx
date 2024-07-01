753b1734e637f50cd9908460e8071d3c42f5d04d
Niek de Wit <niekdewit@live.nl>
2024-07-01 23:05:03 +0200

    add lang to html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout(props: Readonly<{
 }>) {
   const bodyClassName = `h-full w-full m-0 p-0 ${courier.className}`
   return (
-    <html className="h-full w-full m-0 p-0" >
+    <html className="h-full w-full m-0 p-0" lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="icon" href="/favicon.svg" sizes="any" />
