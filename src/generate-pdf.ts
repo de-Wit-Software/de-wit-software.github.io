64f208e6cef241fcbef61e02e9aa3beb7bf4c736
Niek de Wit <niekdewit@live.nl>
2024-09-10 11:04:33 +0200

    add pdf metadata

--- src/generate-pdf.ts
+++ src/generate-pdf.ts
@@ -1,6 +1,9 @@
 import * as fs from 'fs';
 import path from 'path';
 import puppeteer from 'puppeteer';
+const exiftool = require('node-exiftool')
+const exiftoolBin = require('dist-exiftool')
+const ep = new exiftool.ExiftoolProcess(exiftoolBin)
 
 const generate = async function(lang: 'en' | 'nl') {
   let content = fs.readFileSync(
@@ -26,7 +29,7 @@ const generate = async function(lang: 'en' | 'nl') {
   content = content.replace('/profile.webp', imageBase64);
 
   await page.setContent(content)
-
+  
   const buffer = await page.pdf({
       format: 'A4',
       printBackground: true,
@@ -38,9 +41,103 @@ const generate = async function(lang: 'en' | 'nl') {
 
   await browser.close()
 
-  fs.writeFile("docs/dewitsoftware-"+lang+".pdf", buffer, (err) => {
+  
+  const filename = "docs/dewitsoftware-"+lang+".pdf";
+  fs.writeFile(filename, buffer, (err) => {
     if (err) throw err;
-    console.log("The file " +lang+ " has been saved!");
+    console.log("The file " +filename+ " has been saved!");
+
+    const data = {
+      Title: 'de Wit Software - Resume - ' + lang,
+      Author: 'Name: Niek de Wit - Mail (preferred method of communication): niek@dewitsoftware.nl - Phone/Whatsapp: +31610566371',
+      'Subject+': 'A versatile professional full-stack web developer with a strong passion for embedded software, hardware design, and innovative projects, committed to delivering effective technical solutions through expertise in web development and hands-on experience across multiple platforms.',
+      'Keywords+': [
+        'AWS',
+        'Accessibility (WCAG)',
+        'Agile/Scrum',
+        'Angular V2+',
+        'ArgoCD',
+        'Asynchronous Programming',
+        'CI/CD (Continuous Integration/Continuous Deployment)',
+        'CSS',
+        'Cloudflare',
+        'Code Reviews',
+        'Cross-Browser Compatibility',
+        'Docker',
+        'Dynamic web maps',
+        'Expo',
+        'Express.js',
+        'GCP',
+        'Git (Version control)',
+        'GitOps',
+        'Github Actions',
+        'GraphQL',
+        'HTML',
+        'HUGO',
+        'Jasmine',
+        'JavaScript',
+        'Karma',
+        'Kubernetes',
+        'Microservices',
+        'MySQL',
+        'NPM',
+        'NestJS',
+        'NoSQL',
+        'Node.js',
+        'Performance Optimization',
+        'Postgres',
+        'RESTful APIs',
+        'RXJS/NGXS/NgRx',
+        'React Native',
+        'Responsive design',
+        'SASS',
+        'SEO Best Practices',
+        'Stripe',
+        'TypeORM',
+        'Typescript',
+        'UX/UI Best Practices',
+        'Unit Testing',
+        'Unity',
+        'Web Security',
+        'JSON',
+        'XML',
+        'TDD (Test-Driven Development)',
+        'Automation',
+        'Cloud Computing',
+        'Serverless',
+        'Containerization',
+        'API Development',
+        'Frontend Development',
+        'Backend Development',
+        'Wireframing',
+        'Java',
+        'C#',
+        'API Integration',
+        'Software Architecture',
+        'Security/Encryption',
+        'Networking (TCP/IP, DNS)',
+        'SaaS (Software as a Service)',
+        'PaaS (Platform as a Service)',
+        'IaaS (Infrastructure as a Service)',
+        'Infrastructure as Code (IaC)',
+        'Analytics',
+      ],
+      Creator: 'dewitsoftware.nl',
+      Producer: 'de Wit Software Portfolio Generator',
+      Language: lang
+    }
+
+    ep
+      .open()
+      // read and write metadata operations
+      .then(() => ep.writeMetadata(filename, data, ['overwrite_original']))
+      .then(() => ep.readMetadata(filename, ['-File:all']))
+      .then((res: any) => {
+          console.log(JSON.stringify(res))
+      })
+      .then(() => ep.close())
+      .then(() => console.log('Closed exiftool'))
+      .catch(console.error)
   });
 
 }
